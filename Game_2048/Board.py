964ee80743ffa1caeddb6cadf298942d91002921
k1997y <k1997y@icloud.com>
2021-07-18 13:27:31 +0900

    初期状態の盤面描画が可能に

--- Game_2048/Board.py
+++ Game_2048/Board.py
@@ -1,14 +1,17 @@
 import pygame
-import Tile
 import numpy as np
 import random
 
 class Board():
     # 定数
-    WIDTH = 400 # 盤面の幅
-    HEIGHT = 400 # 盤面の高さ
+    BOARD_WIDTH = 500 # 盤面の幅
+    BOARD_HEIGHT = 500 # 盤面の高さ
+    TILE_WIDTH = 100 # タイルの幅
+    TILE_HEIGHT = 100 # タイルの高さ
+    INTERVAL = 25 # タイル間の隙間
     P_2 = 0.9 # 2の出現確率
     P_4 = 0.1 # 4の出現確率
+
 
     # filenames: ファイルパスのリスト
     def __init__(self):
@@ -22,11 +25,12 @@ class Board():
         self.score = 0
 
         # 盤面の2次元配列の作成
-        #self.tiles = np.empty((4,4))
-        self.tiles = [[None for i in range(4)] for j in range(4)]
-        for i in range(4):
-            for j in range(4):
-                self.tiles[i][j] = Tile.Tile(None)
+        self.tiles = np.zeros((4,4))
+        #self.tiles = [[0 for i in range(4)] for j in range(4)]
+        #for i in range(4):
+        #    for j in range(4):
+        #        #self.tiles[i][j] = Tile.Tile(None)
+        #        self.tiles[i][j] = 0
 
 
         #self.tiles = []
@@ -40,17 +44,17 @@ class Board():
         # ランダムで盤面に2枚のタイルを作成する
         rand_i = random.randrange(0,3,1)
         rand_j = random.randrange(0,3,1)
-        self.tiles[rand_i][rand_j] = Tile.Tile(self.genTileRandomly())
+        #self.tiles[rand_i][rand_j] = Tile.Tile(self.genTileRandomly())
+        self.tiles[rand_i,rand_j]=self.genTileRandomly()
 
         # 1枚目のタイルと被りがあるなら再度他の場所に生成する
         while(True):
             rand_i = random.randrange(0,3,1)
             rand_j = random.randrange(0,3,1)
-            if self.tiles[rand_i][rand_j].number == None:
-                self.tiles[rand_i][rand_j] = Tile.Tile(self.genTileRandomly())
+            if self.tiles[rand_i][rand_j] == 0:
+                #self.tiles[rand_i][rand_j] = Tile.Tile(self.genTileRandomly())
+                self.tiles[rand_i,rand_j]=self.genTileRandomly()
                 break
-
-
 
     # 予め設定された確率で2or4のタイルを出す
     def genTileRandomly(self):
--- Game_2048/Game_2048.py
+++ Game_2048/Game_2048.py
@@ -3,10 +3,12 @@ import Board
 
 # スクリーンサイズ
 #SCREEN = pygame.Rect(0,0,400,400)
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 1024
 
 def main():
     pygame.init()
-    screen=pygame.display.set_mode((400,400))
+    screen = pygame.display.set_mode((1024,1024))
     pygame.display.set_caption("2048")
 
     # 画像パス準備
@@ -26,19 +28,63 @@ def main():
 
     # 盤面準備
     board = Board.Board()
+    screen.fill("#92877d")
+    pygame.display.flip()
+
+    # 盤面描画
+    for i in range(4):
+        for j in range(4):
+            tile = pygame.Rect((j + 1) * board.INTERVAL + j * board.TILE_WIDTH,(i+1)*board.INTERVAL+i*board.TILE_HEIGHT,board.TILE_WIDTH,board.TILE_HEIGHT)
+            color = getColorFromNum(board.tiles[i,j])
+            screen.fill(color,rect=tile)
+
+    pygame.display.flip()
+    clock = pygame.time.Clock()
+
+    while True:
+        clock.tick(60)
 
     # クロック準備
-    clock=pygame.time.Clock()
+    #clock=pygame.time.Clock()
 
     # ゲームループ開始
-    while True:
-        clock.tick(60)  #60fps
+    #while True:
+    #    #clock.tick(60) #60fps
 
-        # 塗りつぶす範囲のRectを作成
-        r = pygame.Rect(0,0,board.WIDTH,board.HEIGHT)
-        color = pygame.Color(169,169,169)
-        screen.fill(color,rect=r)    # 灰色で塗りつぶす
-        
+    #    # 塗りつぶす範囲のRectを作成
+    #    r = pygame.Rect(0,0,board.BOARD_WIDTH,board.BOARD_HEIGHT)
+    #    color = pygame.Color(169,169,169)
+    #    screen.fill(color,rect=r) # 灰色で塗りつぶす
+    #    pygame.display.flip()
+      
+# 数字によって違った色を返す
+def getColorFromNum(number):
+    if number == 0:
+        return pygame.Color("#9e948a")
+    elif number == 2:
+        return pygame.Color("#eee4da")
+    elif number == 4:
+        return pygame.Color("#ede0c8")
+    elif number == 8:
+        return pygame.Color("#f2b179")
+    elif number == 16:
+        return pygame.Color("#f59563")
+    elif number == 32:
+        return pygame.Color("#f67c5f")
+    elif number == 64:
+        return pygame.Color("#f65e3b")
+    elif number == 128:
+        return pygame.Color("#edcf72")
+    elif number == 256:
+        return pygame.Color("#edcc61")
+    elif number == 512:
+        return pygame.Color("#edc850")
+    elif number == 1024:
+        return pygame.Color("#edc53f")
+    elif number == 2048:
+        return pygame.Color("#edc22e")
+
+
 
 if __name__ == "__main__":
     main()
